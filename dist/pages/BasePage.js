a2285a67ab83e34fa259947192cd8d47090ca0da
sang.le <leducsang.10dt2@gmail.com>
2025-11-05 13:40:50 +0700

    chore: Remove dist folder from git tracking
    - Remove build output files from version control
    - dist/ folder is already in .gitignore
    - Build artifacts should be generated locally, not committed

--- dist/pages/BasePage.js
+++
@@ -1,9 +0,0 @@
-import SanElement from '../core/SanElement.js';
-export class BasePage {
-    constructor(driver) {
-        this.driver = driver;
-    }
-    $(locator) {
-        return new SanElement(this.driver, locator);
-    }
-}
