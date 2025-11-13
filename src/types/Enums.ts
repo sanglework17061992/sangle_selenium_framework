f090d3fa4f37b0f43edcd3e755255ea314a590a4
sang.le <leducsang.10dt2@gmail.com>
2025-11-13 13:49:27 +0700

    feat: Add core enums for type safety
    - Add BrowserType enum (Chrome, Firefox)
    - Add LogLevel enum (DEBUG, INFO, WARN, ERROR)

---
+++ src/types/Enums.ts
@@ -0,0 +1,17 @@
+/**
+ * Browser types supported by the framework
+ */
+export enum BrowserType {
+  CHROME = 'chrome',
+  FIREFOX = 'firefox'
+}
+
+/**
+ * Log levels for framework logging
+ */
+export enum LogLevel {
+  DEBUG = 'DEBUG',
+  INFO = 'INFO',
+  WARN = 'WARN',
+  ERROR = 'ERROR'
+}
